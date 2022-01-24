4d78894999dcffe646e77fd5e033689c88e88be6
Code Sandbox <hello@codesandbox.io>
2022-01-24 13:23:37 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1ef796d2494a020d98fd60fd3bf15a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3614423ee2289e4fd996401055f9e6",
+    "url": "https://lolthisis.github.io/csb-6eo5l/index.html"
+  },
+  {
+    "revision": "aede9648f7eb0ddfb7a9",
+    "url": "https://lolthisis.github.io/csb-6eo5l/static/css/main.57242a17.chunk.css"
+  },
+  {
+    "revision": "572873ad8043170fe871",
+    "url": "https://lolthisis.github.io/csb-6eo5l/static/js/2.586f8996.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "https://lolthisis.github.io/csb-6eo5l/static/js/2.586f8996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aede9648f7eb0ddfb7a9",
+    "url": "https://lolthisis.github.io/csb-6eo5l/static/js/main.1772f080.chunk.js"
+  },
+  {
+    "revision": "9c391810fed6f0853b99",
+    "url": "https://lolthisis.github.io/csb-6eo5l/static/js/runtime-main.686c2588.js"
+  }
+]);
